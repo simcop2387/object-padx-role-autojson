7c761c2e4a68bb4b531edb26b06657b01716354c
Ryan Voots <simcop2387@simcop2387.info>
2023-11-19 14:37:10 -0500

    Bump version

--- lib/Object/PadX/Role/AutoJSON.pm
+++ lib/Object/PadX/Role/AutoJSON.pm
@@ -35,7 +35,7 @@ Object::Pad::MOP::FieldAttr->register( "JSONNum", permit_hintkey => 'Object::Pad
 Object::Pad::MOP::FieldAttr->register( "JSONStr", permit_hintkey => 'Object::PadX::Role::AutoJSON', apply => $_disallow_value );
 
 # ABSTRACT: Role for Object::Pad that dynamically handles a TO_JSON serialization based on the MOP
-our $VERSION = '1.1';
+our $VERSION = '1.2';
 
 sub import {
   my @imports = @_;
